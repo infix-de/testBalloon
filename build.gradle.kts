fb56ae5914042b894ca6e2d6804f28d15d50e77f
Martin Bonnin <martin@mbonnin.net>
2025-08-20 22:32:34 +0200

    Build: Make sure the build classpath holds into a single classloader (#32)
    * add atomicfu to build-logic
    * replace alias() with id()
    * Consolidate all the build into a single classloader
    * update compat-patrouille

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
-    alias(libs.plugins.org.jetbrains.kotlin.multiplatform) apply false
-    alias(libs.plugins.com.android.application) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
-    alias(libs.plugins.com.vanniktech.maven.publish) apply false
+    id("buildLogic.common").apply(false)
 }
 
 tasks {

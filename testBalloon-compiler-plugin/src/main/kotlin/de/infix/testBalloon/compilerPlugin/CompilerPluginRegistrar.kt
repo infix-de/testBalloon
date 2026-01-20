302e498591229dd5c72a0693f61fb3c094f0899f
OliverO2 <oliver.o456i@gmail.com>
2026-01-20 23:21:04 +0100

    Backport from 0.8.3-K2.3.0 to 0.8.3-K2.2.21

--- testBalloon-compiler-plugin/src/main/kotlin/de/infix/testBalloon/compilerPlugin/CompilerPluginRegistrar.kt
+++ testBalloon-compiler-plugin/src/main/kotlin/de/infix/testBalloon/compilerPlugin/CompilerPluginRegistrar.kt
@@ -1,6 +1,5 @@
 package de.infix.testBalloon.compilerPlugin
 
-import buildConfig.BuildConfig.PROJECT_COMPILER_PLUGIN_ID
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
@@ -8,7 +7,6 @@ import org.jetbrains.kotlin.config.CompilerConfiguration
 
 @OptIn(ExperimentalCompilerApi::class)
 class CompilerPluginRegistrar : CompilerPluginRegistrar() {
-    override val pluginId: String = PROJECT_COMPILER_PLUGIN_ID
     override val supportsK2 = true
 
     override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {

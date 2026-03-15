0b434ced81544f28d9ff58041c1dbb59ef5a46ab
OliverO2 <oliver.o456i@gmail.com>
2026-03-15 14:54:17 +0100

    @TestElementName: Update ABI and KDoc

--- testBalloon-framework-shared/src/commonMain/kotlin/de/infix/testBalloon/framework/shared/Annotations.kt
+++ testBalloon-framework-shared/src/commonMain/kotlin/de/infix/testBalloon/framework/shared/Annotations.kt
@@ -1,21 +1,21 @@
 package de.infix.testBalloon.framework.shared
 
 /**
- * Designates a class or function registering a test suite or test.
+ * Designates a function registering a test suite or test, or a class registering test suite content.
  */
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.BINARY)
 public annotation class TestRegistering
 
 /**
- * Makes a String parameter receive the fully qualified name of its function's or constructor's caller.
+ * Designates a test-registering function's String parameter as representing the test element's name.
  *
- * This annotation requires a top-level `@[TestRegistering]` function or class constructor. A `@[TestElementName]`
- * parameter must be of type String and have a default value (typically, "").
+ * For a top-level [TestRegistering] function, this implies:
+ * - A `@TestElementName` parameter must have a default value (typically, "").
+ * - If the call site of the function does not supply an actual value for that `@TestElementName` parameter,
+ *   the compiler will insert the caller's fully qualified property name.
  *
- * If the call site of the function or constructor does not supply an actual value for that `@[TestElementName]`
- * parameter, the compiler will insert the caller's fully qualified class or property name.
- *
+ * In addition, the IDE plugin uses this annotation to detect the element name position in a parameter list.
  * The optional [prefix] and [postfix] parameters make the IDE plugin aware of modifications to the element name,
  * so that a valid test element path can be produced by static analysis. Example for a Behavior/Gherkin-style
  * `Scenario` function creating a test suite:
@@ -30,13 +30,16 @@ public annotation class TestRegistering
  *     }
  * }
  * ```
+ *
+ * Note: A test-registering function below the top level, which uses its first parameter for the test element name,
+ * is not required to annotate it with `@TestElementName`.
  */
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.BINARY)
 public annotation class TestElementName(val prefix: String = "", val postfix: String = "")
 
 /**
- * Makes a String parameter receive the simple name of its function's or constructor's caller.
+ * Makes a String parameter receive the simple name of its call-site top-level property.
  *
  * For prerequisites and mechanism, see [TestElementName].
  *
